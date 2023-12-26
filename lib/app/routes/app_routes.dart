b3f79bc0e62d0e1b618724ca4c6e60692a404458
Azis202017 <azisa6980@gmail.com>
2023-12-26 22:30:39 +0700

    [Routes] add detail permintaan routes

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -1,12 +1,34 @@
+// ignore_for_file: constant_identifier_names
+
 part of 'app_pages.dart';
 // DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
 
 abstract class Routes {
   Routes._();
   static const HOME = _Paths.HOME;
+  static const LOGIN = _Paths.LOGIN;
+  static const REGISTER = _Paths.REGISTER;
+  static const CHANGE_PROFILE = _Paths.CHANGE_PROFILE;
+  static const BOTTOM_NAVIGATION_BAR = _Paths.BOTTOM_NAVIGATION_BAR;
+  static const PROFILE = _Paths.PROFILE;
+  static const PRODUCT = _Paths.PRODUCT;
+  static const RIWAYAT = _Paths.RIWAYAT;
+  static const RIWAYAT_PENDAPATAN_UANG = _Paths.RIWAYAT_PENDAPATAN_UANG;
+  static const ADD_PRODUCT = _Paths.ADD_PRODUCT;
+  static const DETAIL_PERMINTAAN = _Paths.DETAIL_PERMINTAAN;
 }
 
 abstract class _Paths {
   _Paths._();
   static const HOME = '/home';
+  static const LOGIN = '/login';
+  static const REGISTER = '/register';
+  static const CHANGE_PROFILE = '/change-profile';
+  static const BOTTOM_NAVIGATION_BAR = '/bottom-navigation-bar';
+  static const PROFILE = '/profile';
+  static const PRODUCT = '/product';
+  static const RIWAYAT = '/riwayat';
+  static const RIWAYAT_PENDAPATAN_UANG = '/riwayat-pendapatan-uang';
+  static const ADD_PRODUCT = '/add-product';
+  static const DETAIL_PERMINTAAN = '/detail-permintaan';
 }
