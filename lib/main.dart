92b1e215452c6f041ca5d6dfc72263ab207469a8
Azis202017 <azisa6980@gmail.com>
2023-12-26 22:29:05 +0700

    Main] add theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:pasar_petani/app/shared/theme/theme.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
-  runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    ),
-  );
-}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await initializeDateFormatting('id_ID', null).then((_) => runApp(
+        GetMaterialApp(
+          title: "Application",
+          initialRoute: AppPages.INITIAL,
+          theme: themeData,
+          getPages: AppPages.routes,
+        ),
+      ));
+} 
