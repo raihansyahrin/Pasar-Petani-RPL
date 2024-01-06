9866000b16036c0ba3610caf59c6dc742c6b76fd
Azis202017 <azisa6980@gmail.com>
2024-01-07 01:38:47 +0700

    login controller

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pasar_petani/app/services/authentication_service.dart';
+import 'package:pasar_petani/app/shared/theme/color.dart';
 
 import '../../../routes/app_pages.dart';
 
@@ -18,7 +20,8 @@ class LoginController extends GetxController {
     }
     return null;
   }
-    String? passwordValidate(String? value) {
+
+  String? passwordValidate(String? value) {
     if (value!.length < 7) {
       return "Password diharuskan lebih dari 7 karakter";
     }
@@ -29,13 +32,35 @@ class LoginController extends GetxController {
   }
 
   void login() async {
+    Get.defaultDialog(
+        title: 'Tunggu sebentar yaa',
+        content: const Center(
+          child: CircularProgressIndicator(),
+        ));
     if (formKey.currentState!.validate()) {
       if (await AuthenticationService().login(
           email: emailController.text, password: passwordController.text)) {
+        Get.back();
+        Get.snackbar(
+          "Success",
+          "Berhasil Masuk",
+          backgroundColor: Colors.green,
+          colorText: Colors.white,
+        );
         Get.offAllNamed(Routes.BOTTOM_NAVIGATION_BAR);
+      } else {
+        Get.back();
+
+        Get.snackbar(
+          "Gagal",
+          "Email dan Password salah",
+          backgroundColor: error,
+          colorText: Colors.white,
+        );
       }
     }
   }
+
   void showPassword() {
     isShowPassword = !isShowPassword;
     update();
